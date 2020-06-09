033f04c4f347804fb6e1bbec40249415f27d7f21
praveen kedaresetti <praveenkedaresetti99@gmail.com>
2020-06-09 19:09:03 +0530

    Multiply Function

--- javaproject11/src/javaproject11/JavaClass4.java
+++ javaproject11/src/javaproject11/JavaClass4.java
@@ -5,6 +5,7 @@ public class JavaClass4 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("praveen");
+                System.out.println("currently pursuing btech 3rd year in nitc");
 
 	}
 
